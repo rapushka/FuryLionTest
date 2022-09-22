369bc8bd4f77912248e8a416236ebaaab99009d9
Rapuška <megarubka44@gmail.com>
2022-09-22 14:01:26 +0300

    нерабочее перемещение gameObject-ов токенов

--- Assets/Code/Environment/Gravity.cs
+++ Assets/Code/Environment/Gravity.cs
@@ -8,19 +8,33 @@ namespace Code.Environment
 	public class Gravity
 	{
 		private Token[,] _tokens;
-		private List<Vector2> _willFall;
+		private List<Vector2Int> _willFall;
 
 		public Gravity()
 		{
-			_willFall = new List<Vector2>();
+			_willFall = new List<Vector2Int>();
 		}
 
 		public Token[,] Apply(Token[,] tokens)
 		{
 			_tokens = (Token[,])tokens.Clone();
+
 			MarkToFalling();
+			FallVertically();
 
 			return _tokens;
+		}
+
+		private void FallVertically()
+		{
+			_willFall.ForEach((i) => Debug.Log(i));
+			foreach (var indexes in _willFall)
+			{
+				_tokens[indexes.x, indexes.y]
+					.transform.Translate(GetTokenBellow(indexes.x, indexes.y).transform.position);
+			}
+			
+			_willFall.ForEach((i) => Debug.Log(i));
 		}
 
 		private void MarkToFalling()
@@ -33,22 +47,24 @@ namespace Code.Environment
 
 					if (token == false
 					    || token.ApplyGravity == false
-					    || _tokens[x + 1, y]
-					    || _willFall.Contains(new Vector2(x, y)))
+					    || GetTokenBellow(x, y)
+					    || _willFall.Contains(new Vector2Int(x, y)))
 					{
 						continue;
 					}
 
-					MarkAlsoAbove(x, y);
+					MarkWithAboveTokens(x, y);
 				}
 			}
 		}
 
-		private void MarkAlsoAbove(int startX, int startY)
+		private Token GetTokenBellow(int x, int y) => _tokens[x + 1, y];
+
+		private void MarkWithAboveTokens(int startX, int startY)
 		{
 			for (var x = startX; Continue(x, startY); x--)
 			{
-				_willFall.Add(new Vector2(x, startY));
+				_willFall.Add(new Vector2Int(x, startY));
 			}
 		}
 
