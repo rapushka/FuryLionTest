0c1933c5344051efd14971ed99e73ecd18298c23
Rapuška <megarubka44@gmail.com>
2022-09-29 20:24:48 +0300

    работающий пул токенов

--- Assets/Code/Gameplay/Tokens/TokensPool.cs
+++ Assets/Code/Gameplay/Tokens/TokensPool.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Code.Infrastructure.IdComponents;
 using UnityEngine;
 using Zenject;
@@ -10,7 +11,7 @@ namespace Code.Gameplay.Tokens
 		private readonly Dictionary<TokenType, Token> _tokenPrefabForType;
 		private readonly TokensRoot _tokensRoot;
 		private readonly Dictionary<TokenType, List<Token>> _createdTokens;
-		
+
 		[Inject]
 		public TokensPool(Dictionary<TokenType, Token> tokenPrefabForType, TokensRoot tokensRoot)
 		{
@@ -19,9 +20,42 @@ namespace Code.Gameplay.Tokens
 			_createdTokens = new Dictionary<TokenType, List<Token>>();
 		}
 
-		public Token CreateTokenOfType(TokenType tokenType, Vector3 position) 
-			=> Object.Instantiate(_tokenPrefabForType[tokenType], position, Quaternion.identity, _tokensRoot.transform);
+		public Token CreateTokenOfType(TokenType tokenType, Vector3 position)
+		{
+			CreateListForType(tokenType);
 
-		public void DestroyToken(Token token) => Object.Destroy(token.gameObject);
+			var token = _createdTokens[tokenType].FirstOrDefault(Disabled);
+
+			if (token == null)
+			{
+				token = InstantiateAtRoot(position, _tokenPrefabForType[tokenType]);
+				_createdTokens[tokenType].Add(token);
+			}
+			else
+			{
+				token.gameObject.SetActive(true);
+				token.transform.position = position;
+			}
+
+			return token;
+		}
+
+		private bool Disabled(Token t) => t.gameObject.activeInHierarchy == false;
+
+		private void CreateListForType(TokenType tokenType)
+		{
+			if (_createdTokens.ContainsKey(tokenType) == false)
+			{
+				_createdTokens.Add(tokenType, new List<Token>());
+			}
+		}
+
+		private Token InstantiateAtRoot(Vector3 position, Token original)
+			=> Object.Instantiate(original, position, Quaternion.identity, _tokensRoot.transform);
+
+		public void DestroyToken(Token token)
+		{
+			token.gameObject.SetActive(false);
+		}
 	}
 }
