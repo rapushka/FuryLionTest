bc2c566dbd0d5fa3a139e2af9ec261bc9016beb2
Rapuška <megarubka44@gmail.com>
2022-10-17 12:01:42 +0300

    выделен базовый класс SingleFileGenerator

--- Assets/Code/Analytics/HandlersGeneration/Handler/HandlerGenerator.cs
+++ Assets/Code/Analytics/HandlersGeneration/Handler/HandlerGenerator.cs
@@ -1,33 +1,15 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using Code.Analytics.GoogleSheetsIntegration;
 
 namespace Code.Analytics.HandlersGeneration.Handler
 {
-	public class HandlerGenerator
+	public class HandlerGenerator : SingleFileGenerator
 	{
-		private List<AnalyticEventHandler> _handlers;
-
-		public void OnDataProcessed(List<AnalyticEventHandler> handlers)
-		{
-			_handlers = handlers;
-
-			const string @namespace = "Code.Generated.Analytics";
-			const string className = "AnalyticEventsHandler";
-
-			var path = $@"{Directory.GetCurrentDirectory()}\Assets\{@namespace.Replace('.', '\\')}";
-			using var file = File.CreateText(path + @$"\{className}.cs");
-
-			var code = GenerateClass(@namespace, className);
-			file.Write(code);
-		}
-
-		private string GenerateClass(string @namespace, string className)
+		protected override string GenerateClass(string @namespace, string className)
 			=> HandlerCodeTemplates.Class(@namespace, className, GenerateMethods());
 
 		private string GenerateMethods() => string.Join('\n', HandlersAsTemplates());
 
-		private IEnumerable<string> HandlersAsTemplates() => _handlers.Select(HandlerCodeTemplates.Method);
+		private IEnumerable<string> HandlersAsTemplates() => Handlers.Select(HandlerCodeTemplates.Method);
 	}
 }
