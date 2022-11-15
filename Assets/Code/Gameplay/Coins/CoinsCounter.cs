b5c5fedf977dfedad8589a5697836e564843cecb
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-15 19:11:06 +0300

    Выделено свойство

--- Assets/Code/Gameplay/Coins/CoinsCounter.cs
+++ Assets/Code/Gameplay/Coins/CoinsCounter.cs
@@ -14,18 +14,24 @@ namespace Code.Gameplay.Coins
 
 		[Inject] public CoinsCounter(SignalBus signalBus) => _signalBus = signalBus;
 
+		private int CurrentCoinsCount
+		{
+			get => _currentCoinsCount;
+			set
+			{
+				_currentCoinsCount = value; 
+				InvokeValueUpdate();
+			}
+		}
+
 		public void Initialize() => InvokeValueUpdate();
 
 		public void OnChainComposed(IEnumerable<Token> chain) => IncreaseCoinsCount(chain.Count());
 
 		public void OnTokensDestroyed(int count) => IncreaseCoinsCount(count);
 
-		private void IncreaseCoinsCount(int count)
-		{
-			_currentCoinsCount += count; // TODO: multiply by CoinsPerToken
-			InvokeValueUpdate();
-		}
+		private void IncreaseCoinsCount(int count) => CurrentCoinsCount += count; // TODO: multiply by CoinsPerToken
 
-		private void InvokeValueUpdate() => _signalBus.Fire(new CoinsCountUpdateSignal(_currentCoinsCount));
+		private void InvokeValueUpdate() => _signalBus.Fire(new CoinsCountUpdateSignal(CurrentCoinsCount));
 	}
 }
