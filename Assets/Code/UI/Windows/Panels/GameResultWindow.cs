2d09eb85851ae0d0f127773876e78123925282a5
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-16 23:59:16 +0300

    +GameResultWindow

---
+++ Assets/Code/UI/Windows/Panels/GameResultWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Code.UI.Windows.Service;
+using TMPro;
+using UnityEngine;
+
+namespace Code.UI.Windows.Panels
+{
+	public class GameResultWindow : UnityWindow
+	{
+		[SerializeField] private TextMeshProUGUI _loseTextMesh;
+		[SerializeField] private TextMeshProUGUI _victoryTextMesh;
+
+		public void Construct(SessionResult sessionResult) => ChoiceTextMesh(sessionResult).enabled = true;
+		
+		private TextMeshProUGUI ChoiceTextMesh(SessionResult sessionResult)
+			=> sessionResult switch
+			{
+				SessionResult.Lose    => _loseTextMesh,
+				SessionResult.Victory => _victoryTextMesh,
+				_                     => throw new ArgumentException(nameof(sessionResult)),
+			};
+	}
+}
