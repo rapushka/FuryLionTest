cbc93847b7fd54c303ec3291a479d84e08a7937c
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-16 23:35:44 +0300

    +WindowsService

---
+++ Assets/Code/UI/Windows/Service/UnityWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Code.UI.Windows.Service
+{
+	public abstract class UnityWindow : MonoBehaviour
+	{
+		[SerializeField] private GameObject _window;
+
+		public WindowResult Result { get; protected set; }
+		public virtual void Open() => _window.SetActive(true);
+		public virtual void Hide() => _window.SetActive(false);
+	}
+}
---
+++ Assets/Code/UI/Windows/Service/WindowsService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Code.UI.Windows.Service
+{
+	public class WindowsService : MonoBehaviour
+	{
+		[SerializeField] private List<UnityWindow> _windows;
+
+		private Dictionary<Type, UnityWindow> _windowsDictionary;
+		private Stack<UnityWindow> _windowsStack;
+
+		private bool HasOpenedWindow => _windowsStack.Any();
+
+		private void OnEnable() => _windowsDictionary = _windows.ToDictionary((w) => w.GetType());
+
+		public void OpenNew<TWindow>(Action<TWindow> initializeWindow = null)
+			where TWindow : UnityWindow
+		{
+			var window = GetWindowOfType<TWindow>();
+			HideOpenedWindow();
+
+			_windowsStack.Push(window);
+			initializeWindow?.Invoke((TWindow)window);
+			window.Open();
+		}
+
+		public void Close(Action<WindowResult> onClosed = null)
+		{
+			var window = _windowsStack.Pop();
+			window.Hide();
+			onClosed?.Invoke(window.Result);
+
+			if (HasOpenedWindow)
+			{
+				_windowsStack.Peek().Open();
+			}
+		}
+
+		private UnityWindow GetWindowOfType<TWindow>()
+			where TWindow : UnityWindow
+		{
+			if (_windowsDictionary.TryGetValue(typeof(TWindow), out var window) == false)
+			{
+				throw new Exception($"Window {typeof(TWindow).Name} is not found");
+			}
+
+			return window;
+		}
+
+		private void HideOpenedWindow()
+		{
+			if (HasOpenedWindow)
+			{
+				_windowsStack.Peek().Hide();
+			}
+		}
+	}
+}
