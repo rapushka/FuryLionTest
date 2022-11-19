ed3ebce5aae65075ae32260dea06f06c905cf972
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-19 23:08:40 +0300

    Локализации 3 вариантов цели

--- Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
+++ Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
@@ -1,18 +1,35 @@
 ﻿using Code.GameLoop.Goals.Conditions;
+using TMPro;
 using UnityEngine;
 
 namespace Code.UI.Windows.Panels
 {
 	public class QuestCompletedWindow : UnityWindow, IGoalVisitor
 	{
-		[SerializeField] private GameObject _destroyAllObstaclesOfTypeView;
-		[SerializeField] private GameObject _destroyNTokensOfColorView;
-		[SerializeField] private GameObject _reachScoreValueView;
+		[SerializeField] private TextMeshProUGUI _destroyAllObstaclesOfTypeView;
+		[SerializeField] private TextMeshProUGUI _destroyNTokensOfColorView;
+		[SerializeField] private TextMeshProUGUI _reachScoreValueView;
 		
-		public void Visit(DestroyAllObstaclesOfType goal) => _destroyAllObstaclesOfTypeView.SetActive(true);
+		public void Visit(DestroyAllObstaclesOfType goal)
+		{
+			var tokenUnit = goal.Type;
+			_destroyAllObstaclesOfTypeView.gameObject.SetActive(true);
+			_destroyAllObstaclesOfTypeView.text = string.Format(_destroyAllObstaclesOfTypeView.text, tokenUnit);
+		}
 
-		public void Visit(DestroyNTokensOfColor goal) => _destroyNTokensOfColorView.SetActive(true);
+		public void Visit(DestroyNTokensOfColor goal)
+		{
+			var targetCount = goal.TargetCount;
+			var color = goal.Color;
+			_destroyNTokensOfColorView.gameObject.SetActive(true);
+			_destroyNTokensOfColorView.text = string.Format(_destroyNTokensOfColorView.text, targetCount, color);
+		}
 
-		public void Visit(ReachScoreValue goal) => _reachScoreValueView.SetActive(true);
+		public void Visit(ReachScoreValue goal)
+		{
+			var targetScore = goal.TargetScoreValue;
+			_reachScoreValueView.gameObject.SetActive(true);
+			_reachScoreValueView.text = string.Format(_reachScoreValueView.text, targetScore);
+		}
 	}
 }
