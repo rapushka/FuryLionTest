c68ebd0cef1dd8d875d03c95a79a167d684b12a7
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-20 16:56:39 +0300

    Fixed

--- Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
+++ Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
@@ -1,30 +1,56 @@
-﻿using Code.DataStoring.Localizations.LocalsVariables;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Code.DataStoring.Localizations.LocalsVariables;
 using Code.Extensions;
 using Code.GameLoop.Goals.Conditions;
 using Code.GameLoop.Goals.Progress.ProgressObservers;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Localization;
+using UnityEngine.UI;
 
 namespace Code.UI.Windows.Panels
 {
 	public class QuestCompletedWindow : UnityWindow, IGoalVisitor
 	{
+		[SerializeField] private Button _buttonClose;
 		[SerializeField] private TextMeshProUGUI _textMesh;
-		[SerializeField] private LocalizedString _obstaclesLocalizedString;
-		[SerializeField] private LocalizedString _tokensLocalizedString;
-		[SerializeField] private LocalizedString _scoreLocalizedString;
+		[SerializeField] private LocalizedString _obstaclesString;
+		[SerializeField] private LocalizedString _tokensString;
+		[SerializeField] private LocalizedString _scoreString;
 		[SerializeField] private Values _values;
 
-		public void Initialize(ProgressObserver progressObserver) => progressObserver.Goal.Accept(this);
+		private readonly Stack<string> _stack = new();
+
+		private void Start() => _buttonClose.onClick.AddListener(UserClose);
+
+		private void OnDestroy() => _buttonClose.onClick.RemoveListener(UserClose);
+
+		private void UserClose()
+		{
+			if (_stack.Any())
+			{
+				_stack.Pop();
+			}
+
+			ActualizeText();
+		}
+
+		public void Initialize(ProgressObserver progressObserver)
+		{
+			progressObserver.Goal.Accept(this);
+			ActualizeText();
+		}
 
 		public void Visit(DestroyAllObstaclesOfType goal)
-			=> _textMesh.text = _obstaclesLocalizedString.GetLocalizedString(goal.Type.GetName());
+			=> _stack.Push(_obstaclesString.GetLocalizedString(goal.Type.GetName()));
 
 		public void Visit(DestroyNTokensOfColor goal)
-			=> _textMesh.text = _tokensLocalizedString.GetLocalizedString(_values.Set(goal.TargetCount, goal.Color));
+			=> _stack.Push(_tokensString.GetLocalizedString(_values.Set(goal.TargetCount, goal.Color)));
 
-		public void Visit(ReachScoreValue goal)
-			=> _textMesh.text = _scoreLocalizedString.GetLocalizedString(goal.TargetScoreValue);
+		public void Visit(ReachScoreValue goal) => _stack.Push(_scoreString.GetLocalizedString(goal.TargetScoreValue));
+		
+		private void ActualizeText() => _textMesh.text = _stack.Any() ? _stack.Peek() : string.Empty;
 	}
 }
