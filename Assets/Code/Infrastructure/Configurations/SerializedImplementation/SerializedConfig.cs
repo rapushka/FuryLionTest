05f9fe1dd9379c3eb70997e147db41fbd7d6b08a
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-20 17:38:32 +0300

    Переименования Конфигов

--- Assets/Code/Infrastructure/Configurations/SerializedImplementation/SerializedConfig.cs
+++ Assets/Code/Infrastructure/Configurations/SerializedImplementation/SerializedConfig.cs
@@ -5,34 +5,34 @@ namespace Code.Infrastructure.Configurations.SerializedImplementation
 {
 	public class SerializedConfig : MonoBehaviour, IConfig
 	{
-		[Space] [SerializeField] private SerializedFieldConfig _serializedFieldConfig;
-		[Space] [SerializeField] private SerializedChainConfig _serializedChainConfig;
-		[Space] [SerializeField] private SerializedInputConfig _serializedInputConfig;
-		[Space] [SerializeField] private SerializedScoreConfig _serializedScoreConfig;
-		[Space] [SerializeField] private SerializedBonusesConfig _serializedBonusesConfig;
+		[Space] [SerializeField] private SerializedFieldConfig _field;
+		[Space] [SerializeField] private SerializedChainConfig _chain;
+		[Space] [SerializeField] private SerializedInputConfig _input;
+		[Space] [SerializeField] private SerializedScoreConfig _score;
+		[Space] [SerializeField] private SerializedBonusesConfig _bonuses;
 		[Space] [SerializeField] private SerializedCoinsConfig _coins;
 
-		int IChainConfig.MinTokensCountForChain => _serializedChainConfig.MinTokensCountForChain;
+		int IChainConfig.MinTokensCountForChain => _chain.MinTokensCountForChain;
 
-		float IInputConfig.CursorOverlapRadius => _serializedInputConfig.CursorOverlapRadius;
+		float IInputConfig.CursorOverlapRadius => _input.CursorOverlapRadius;
 
-		float IFieldConfig.Step => _serializedFieldConfig.Step;
+		float IFieldConfig.Step => _field.Step;
 
-		Vector2 IFieldConfig.Offset => _serializedFieldConfig.Offset;
+		Vector2 IFieldConfig.Offset => _field.Offset;
 
-		Vector2Int IFieldConfig.FieldSizes => _serializedFieldConfig.FieldSizes;
+		Vector2Int IFieldConfig.FieldSizes => _field.FieldSizes;
 
-		int IScoreConfig.ScoreMultiplier => _serializedScoreConfig.ScoreMultiplier;
+		int IScoreConfig.ScoreMultiplier => _score.ScoreMultiplier;
 
-		float IScoreConfig.MultiplierPerTokenInChain => _serializedScoreConfig.MultiplierPerTokenInChain;
+		float IScoreConfig.MultiplierPerTokenInChain => _score.MultiplierPerTokenInChain;
 
-		int IBonusesConfig.MinChainLenghtForRocket => _serializedBonusesConfig.MinChainLenghtForRocket;
+		int IBonusesConfig.MinChainLenghtForRocket => _bonuses.MinChainLenghtForRocket;
 
-		int IBonusesConfig.MaxChainLenghtForRocket => _serializedBonusesConfig.MaxChainLenghtForRocket;
+		int IBonusesConfig.MaxChainLenghtForRocket => _bonuses.MaxChainLenghtForRocket;
 
-		int IBonusesConfig.MinChainLenghtForBomb => _serializedBonusesConfig.MinChainLenghtForBomb;
+		int IBonusesConfig.MinChainLenghtForBomb => _bonuses.MinChainLenghtForBomb;
 
-		int IBonusesConfig.BombExplosionRange => _serializedBonusesConfig.BombExplosionRange;
+		int IBonusesConfig.BombExplosionRange => _bonuses.BombExplosionRange;
 
 		int ICoinsConfig.CoinsPerToken         => _coins.CoinsPerToken;
 
