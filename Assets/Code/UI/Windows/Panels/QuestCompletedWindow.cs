c680d7a6d6a00b5c71b45a290f809fde6aa2654f
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-20 16:13:03 +0300

    Исправлен баг локализации
    с передачей нескольких аргументов в строку

--- Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
+++ Assets/Code/UI/Windows/Panels/QuestCompletedWindow.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using Code.DataStoring.Localizations.LocalsVariables;
 using Code.Extensions;
 using Code.GameLoop.Goals.Conditions;
 using Code.GameLoop.Goals.Progress.ProgressObservers;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Localization;
 
@@ -15,6 +14,7 @@ namespace Code.UI.Windows.Panels
 		[SerializeField] private LocalizedString _obstaclesLocalizedString;
 		[SerializeField] private LocalizedString _tokensLocalizedString;
 		[SerializeField] private LocalizedString _scoreLocalizedString;
+		[SerializeField] private Values _values;
 
 		public void Initialize(ProgressObserver progressObserver) => progressObserver.Goal.Accept(this);
 
@@ -22,7 +22,7 @@ namespace Code.UI.Windows.Panels
 			=> _textMesh.text = _obstaclesLocalizedString.GetLocalizedString(goal.Type.GetName());
 
 		public void Visit(DestroyNTokensOfColor goal)
-			=> _textMesh.text = _tokensLocalizedString.GetLocalizedString(goal.TargetCount, goal.Color.GetName());
+			=> _textMesh.text = _tokensLocalizedString.GetLocalizedString(_values.Set(goal.TargetCount, goal.Color));
 
 		public void Visit(ReachScoreValue goal)
 			=> _textMesh.text = _scoreLocalizedString.GetLocalizedString(goal.TargetScoreValue);
