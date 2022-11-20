9a3e64d948a385764b2f844100a96e81f052afab
Rapuška <mikalajsiatkouski@gmail.com>
2022-11-20 17:24:01 +0300

    Removed Reducant Extensions

--- Assets/Code/Extensions/EnumExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Code.Extensions
-{
-	public static class EnumExtensions
-	{
-		public static string GetName(this Enum value) => Enum.GetName(value.GetType(), value);
-	}
-}
